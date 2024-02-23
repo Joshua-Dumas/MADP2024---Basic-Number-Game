47fe0c5560c1a726df083b599be54891142d0ac7
Joshua-Dumas <joshua.dumas45@gmail.com>
2024-02-23 09:38:40 -0600

    LearnGame Branch

--- app/src/main/java/com/example/bignumber/MainActivity.kt
+++ app/src/main/java/com/example/bignumber/MainActivity.kt
@@ -3,78 +3,112 @@ package com.example.bignumber
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.ListView
 import android.widget.TextView
-import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import java.util.Random
 
+data class WordDefinition(val word: String, val definition: String);
+
 class MainActivity : AppCompatActivity() {
+    private var dataDefList = ArrayList<String>();
+    private lateinit var myAdapter : ArrayAdapter<String>;
     private var leftNum :Int = 0;
     private var rightNum :Int = 0;
     private var score :Int = 0;
 
+    private var wordDefinitions = mutableListOf<WordDefinition>();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // above init our app ui
 
+
+        loadDefinitions();
         pickRandomNumbers();
+        setupList();
 
+        val def_list = findViewById<ListView>(R.id.dynamic_def_list);
+        def_list.setOnItemClickListener { _, _, index, _ ->
+            dataDefList.removeAt(index);
+            myAdapter.notifyDataSetChanged();
+        };
     }
 
-    fun leftButtonClick(view: View)
+    fun setupList()
     {
-        if(leftNum > rightNum)
-            score++;
-        else
-            score--;
+        refreshWordAndDefinitions()
 
-        updateScoreText();
+        myAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataDefList);
+
+        val def_list = findViewById<ListView>(R.id.dynamic_def_list);
+        def_list.adapter = myAdapter;
+    }
+
+    fun loadDefinitions()
+    {
+        wordDefinitions.add(WordDefinition("simple","easy to understand"));
+
+        wordDefinitions.add(WordDefinition("game engine","library that manages the lifetime of the game"));
+
+        wordDefinitions.add(WordDefinition("kotlin","programming language"));
+
+        wordDefinitions.add(WordDefinition("Love2D","game framework for lua"));
+    }
+
+    fun refreshWordAndDefinitions()
+    {
+        findViewById<TextView>(R.id.word_id).text = wordDefinitions[0].word;
+
+        dataDefList.clear();
+
+        for(wd in wordDefinitions){
+            dataDefList.add(wd.definition)
+        }
+
+        dataDefList.shuffle()
+    }
+
+    fun radioButtonOnClick(view: View)
+    {
+        /*if (view.id == R.id.rb_one)
+        {
+            var tv = findViewById<TextView>(R.id.HelloText);
+            tv.text = "Radio Button One";
+        }
+
+        if (view.id == R.id.rb_two)
+        {
+            var tv = findViewById<TextView>(R.id.HelloText);
+            tv.text = "Radio Button Two";
+        }*/
+    }
+
+    fun leftButtonOnClick(view: View)
+    {
         pickRandomNumbers();
     }
 
-    fun rightButtonClick(view: View)
+    fun rightButtonOnClick(view: View)
     {
-        if(leftNum < rightNum)
-            score++;
-        else
-            score--;
-
-        updateScoreText();
         pickRandomNumbers();
     }
 
-    private fun pickRandomNumbers()
+    fun pickRandomNumbers()
     {
-        val leftButton = findViewById<Button>(R.id.left_number_button);
-        val rightButton = findViewById<Button>(R.id.right_number_button);
+        /*var leftButton = findViewById<Button>(R.id.left_number_button);
+        var rightButton = findViewById<Button>(R.id.right_number_button);
 
-        val rand = Random()
+        var rand = Random();
 
         leftNum = rand.nextInt(10);
         rightNum = rand.nextInt(10);
 
         leftButton.text = "$leftNum";
-        rightButton.text = "$rightNum";
+        rightButton.text = "$rightNum";*/
     }
 
-    private fun updateScoreText()
-    {
-        val scoreText = findViewById<TextView>(R.id.score_text);
-        val scoreString: String = scoreText.text.toString().drop(7);
-
-        if (scoreString.toInt() > score)
-            Toast.makeText(this, "OOO NO", Toast.LENGTH_SHORT).show();
-        else if (scoreString.toInt() < score)
-            Toast.makeText(this, "YOU GOT IT!!", Toast.LENGTH_SHORT).show();
-
-        val winImage = findViewById<ImageView>(R.id.win_image)
-
-        if (score >= 5)
-            winImage.visibility = View.VISIBLE
-        else
-            winImage.visibility = View.INVISIBLE
-
-        scoreText.text = "Score: $score";
-    }
 }
